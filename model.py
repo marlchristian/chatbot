261e330efe682c4a88920c44617e44b5418cda69
marlchristian <95300127+marlchristian@users.noreply.github.com>
2023-12-10 23:16:36 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,20 +1,25 @@
-import streamlit as st
+port streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from fuzzywuzzy import process
 import warnings
 
+# Suppressing warnings for cleaner output
 warnings.simplefilter('ignore')
 
+# Load data
 moviesA = pd.read_csv('https://raw.githubusercontent.com/Koersken/recommender/main/imdb_top_1000.csv')
 moviesB = pd.read_csv('https://raw.githubusercontent.com/Koersken/recommender/main/Filipino_Movies_Letterbox_Koersken.csv')
 
+# Data preprocessing
 moviesA = moviesA.rename(columns={'Series_Title': 'title', 'Overview': 'desc'})
 moviesA['desc'] = moviesA['desc'] + moviesA['Genre']
 moviesA = moviesA.drop(columns=['Poster_Link', 'Released_Year', 'Certificate', 'Runtime', 'Genre', 'IMDB_Rating', 'Meta_score', 'Director', 'Star1', 'Star2', 'Star3', 'Star4', 'No_of_Votes', 'Gross'])
 
 moviesB = moviesB.drop_duplicates(subset='title')
 
+# TF-IDF vectorization
 tfidf_vectorizer = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0.0, stop_words='english')
 tfidf_matrix = tfidf_vectorizer.fit_transform(moviesB['desc'])
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
@@ -23,21 +28,22 @@ moviesA = moviesA.reset_index()
 titles = moviesA['title']
 indices = pd.Series(moviesA.index, index=moviesA['title'])
 
+# Function to get movie recommendations
 def get_recommendations(title):
-    if title in indices:
-        idx_A = indices[title]
+    match, score = process.extractOne(title, indices.keys())
+    idx_A = indices[match]
 
-        sim_scores = list(enumerate(cosine_sim[:, idx_A]))
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-        sim_scores = sim_scores[1:11]
-        movie_indices = [i[0] for i in sim_scores]
+    sim_scores = list(enumerate(cosine_sim[:, idx_A]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
 
-        return moviesB.iloc[movie_indices]['title']
-    else:
-        return "Movie not found in the database."
+    return moviesB.iloc[movie_indices]['title']
 
-st.title('Movie Recommender: Western to Filipino')
+# Streamlit app
+st.title('Movie Recommender')
 
+# User input for movie title
 user_input = st.text_input("Enter a movie title:", '')
 
 if st.button('Get Recommendations'):
