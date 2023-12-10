b15089bc0e8c8bdab862cf09416a3bd17dc3b4d5
marlchristian <95300127+marlchristian@users.noreply.github.com>
2023-12-10 20:30:29 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+import time
+import asyncio
+import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
