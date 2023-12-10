ed5fc4878c70198aef766d846ca5f341abacd05b
marlchristian <95300127+marlchristian@users.noreply.github.com>
2023-12-10 23:42:17 +0800

    Update model.py

--- model.py
+++ model.py
@@ -28,8 +28,8 @@ moviesA = moviesA.reset_index()
 titles = moviesA['title']
 indices = pd.Series(moviesA.index, index=moviesA['title'])
 
-# Function to get movie recommendations
-def get_recommendations(title):
+# Function to get movie recommendations with descriptions
+def get_recommendations_with_desc(title):
     match, score = process.extractOne(title, indices.keys())
     idx_A = indices[match]
 
@@ -38,8 +38,9 @@ def get_recommendations(title):
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
 
-    return moviesB.iloc[movie_indices]['title', 'desc']
-
+    recommended_movies = moviesB.iloc[movie_indices][['title', 'desc']]
+    return recommended_movies
+    
 # Streamlit app
 st.title('Movie Recommender: Filipino to Western')
 
@@ -47,6 +48,9 @@ st.title('Movie Recommender: Filipino to Western')
 user_input = st.text_input("Enter a movie title:", '')
 
 if st.button('Get Recommendations'):
-    recommendations = get_recommendations(user_input)
-    st.write("Recommended Movies:")
-    st.write(recommendations)
+    recommended_movies = get_recommendations_with_desc(user_input)
+    st.write("Recommended Movies with Descriptions:")
+    for index, row in recommended_movies.iterrows():
+        st.write(f"Title: {row['title']}")
+        st.write(f"Description: {row['desc']}")
+        st.write("-" * 50)
